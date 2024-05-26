e2d475b01d73f10812bb352173b09183722699fa
RivYash <rivankaryash2@gmail.com>
2024-05-27 01:22:41 +0530

    final-commit addeed new template

---
+++ header.php
@@ -0,0 +1,80 @@
+ <!-- ======= Header ======= -->
+ <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex justify-content-between">
+
+      <div class="logo">
+        <!-- Uncomment below if you prefer to use an text logo -->
+        <!-- <h1><a href="index.html">NewBiz</a></h1> -->
+        <a href="index.php"><img src="assets/img/logo.png" alt="" class="img-fluid" style="max-height: 50px;"></a>
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="#services">Services</a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
+          <li><a class="nav-link scrollto" href="#team">Team</a></li>
+          <li class="dropdown"><a href="#"><span>Calculate CS</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li class="dropdown"><a href="#"><span>IIM's</span> <i class="bi bi-chevron-right"></i></a>
+                <ul class="dropdown-menu-scroll">
+                  <li><a href="#section2">IIM Ahmedabad</a></li>
+                  <li><a href="#section3">IIM Banglore</a></li>
+                  <li><a href="#section3">IIM Culcutta</a></li>
+                  <li><a href="#section3">IIM Indore</a></li>
+                  <li><a href="#section3">IIM Lucknow</a></li>
+                  <li><a href="#section3">IIM Kozhikode</a></li>
+                  <li><a href="#section3">IIM Shillong</a></li>
+                  <li><a href="#section3">IIM Mumbai</a></li>
+                  <li><a href="#section3">IIM Rohtak</a></li>
+                  <li><a href="#section3">IIM Udaipur</a></li>
+                  <li><a href="#section3">IIM Trichy</a></li>
+                  <li><a href="#section3">IIM Raipur</a></li>
+                  <li><a href="#section3">IIM Ranchi</a></li>
+                  <li><a href="#section3">IIM Rohtak</a></li>
+                  <li><a href="#section3">IIM Kashipur</a></li>
+                  <li><a href="#section3">IIM Vizag</a></li>
+                  <li><a href="#section3">IIM Nagpur</a></li>
+                  <li><a href="#section3">IIM Amritsar</a></li>
+                  <li><a href="#section3">IIM Sambhalpur</a></li>
+                  <li><a href="#section3">IIM Sirmaur</a></li>
+                  <li><a href="#section3">IIM Bodhgaya</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a href="#"><span>IIT's</span> <i class="bi bi-chevron-right"></i></a>
+                <ul class="dropdown-menu-scroll">
+                  <li><a href="#">IIT Bombay</a></li>
+                  <li><a href="#">IIT Delhi</a></li>
+                  <li><a href="#">IIT Kharagpur</a></li>
+                  <li><a href="#">IIT Madraas</a></li>
+                  <li><a href="#">IIT Kanpur</a></li>
+                  <li><a href="#">IIT Roorkee</a></li>
+                  <li><a href="#">IIT Dhanbad</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a href="#"><span>Other BSchools</span> <i class="bi bi-chevron-right"></i></a>
+                <ul class="dropdown-menu-scroll">
+                  <li><a href="#">FMS Delhi</a></li>
+                  <li><a href="#">MDI Gurgaon</a></li>
+                  <li><a href="#">IMT Gaziabad</a></li>
+                  <li><a href="#">GIM Goa</a></li>
+                  <li><a href="#">GLIM Chennai</a></li>
+                  <li><a href="#">FORE Delhi</a></li>
+                  <li><a href="#">NMIMS Mumbai</a></li>
+                  <li><a href="#">TAPMI Manipal</a></li>
+                  <li><a href="#">KJ Somaiyya Mumbai</a></li>
+                  <li><a href="#">NIBM Pune</a></li>
+                  <li><a href="#">IIFT Kolkata</a></li>
+                </ul>
+              </li>
+              <!-- <li><a href="#">Drop Down 4</a></li> -->
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- #header -->
